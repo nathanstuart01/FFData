62b4ec70cdbbf68f9de4bd695349bdb842563f81
Nathan Stuart <nathan.stuart@ascentialedge.com>
2019-11-05 09:46:26 -0700

    added methods to deteremine potential tds by player

--- Player.java
+++ Player.java
@@ -2,23 +2,71 @@ import java.lang.Math;
 
 public class Player {
 
+    String weeklyPosition;
+    double teamPoints;
+
+    public Player(String position, double teamPPG) {
+        weeklyPosition = position;
+        teamPoints = teamPPG;
+    }
+
     //method that takes over under line and determines this players teams implied point total
     //static double impliedTeamPointTotals
 
     //method that calculates range of outcomes of opposing defense to one standard deviation
 
-    //method that takes average points scored per team and divides by six to show how many potetial tds can be scored
-    // data source espn.com avg points per game eventually will crawl this
-    static double teamTouchdownsScoredPerGame(double teamPPG) {
-        double teamTouchdowns = Math.round((teamPPG / 6) * 10.0) / 10.0;
-        return teamTouchdowns;
+    // put in a stat about touches/catches/percentage of snaps to show scoring potential
+
+    //method that calculates the number of touchdowns this player may score based on their weekly position(rb1, rb2, etc. etc.)
+    public double potentialTouchdowns(double teamPPG, String playerPosition) {
+        // teamppg is the average points scored by the whole team
+        double teamTouchdowns = teamPPG / 6;
+        double potentialTDs = 0;
+
+        //weighted average values by position
+        double rb1 = 0.25;
+        double rb2 = 0.15;
+        double wr1 = 0.25;
+        double wr2 = 0.15;
+        double te = 0.08;
+        double qb = 0.02;
+        double flex = 0.10;
+
+        switch(playerPosition) {
+            case "rb1":
+                potentialTDs = teamTouchdowns * rb1;
+                break;
+            case "rb2":
+                potentialTDs = teamTouchdowns * rb2;
+                break;
+            case "wr1":
+                potentialTDs = teamTouchdowns * wr1;
+                break;
+            case "wr2":
+                potentialTDs = teamTouchdowns * wr2;
+                break;
+            case "te":
+                potentialTDs = teamTouchdowns * te;
+                break;
+            case "qb":
+                potentialTDs = teamTouchdowns * qb;
+            case "flex":
+                potentialTDs = teamTouchdowns * flex;
+                break;
+        }
+        return potentialTDs;
     }
 
     // make a method that then shows how many td's this player could be expected to score
-
     public static void main(String[] args) {
-        double test = Player.teamTouchdownsScoredPerGame(12);
-        System.out.println(test);
+        Player robertWoods = new Player("wr2", 26.8);
+        double potentialTDs = robertWoods.potentialTouchdowns(robertWoods.teamPoints, robertWoods.weeklyPosition);
+        System.out.println("Robert woods as wr2 potential tds: " + potentialTDs);
+
+        Player zacPascal = new Player("wr1", 22.8);
+        double potentialTDs2 = zacPascal.potentialTouchdowns(zacPascal.teamPoints, zacPascal.weeklyPosition);
+        System.out.println("Zac Pascal as wr1 potential tds: " + potentialTDs2);
     }
+
 
 } 
